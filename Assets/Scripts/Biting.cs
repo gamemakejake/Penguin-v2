1a99e4a698a904a1b00e781b47671499d672e6cc
kevincbryan <kevincbryan@gmail.com>
2020-05-05 14:45:43 -0700

    Added biting to penguin

---
+++ Assets/Scripts/Biting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Biting : MonoBehaviour
+{
+   public GameObject maw;
+   public bool isMaw;
+
+   private void Update() 
+   {
+       if (Input.GetButtonDown("Fire1") && isMaw == false)
+       {
+           isMaw = true;
+           maw.SetActive(true);
+           Invoke ("CloseMaw", .5f);
+       }
+
+   }
+
+   void CloseMaw ()
+   {
+       isMaw = false;
+       maw.SetActive(false);
+   }
+}
