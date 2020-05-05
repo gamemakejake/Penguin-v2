3bf5f928b46369e387777ad337ddca7b01166fe1
kevincbryan <kevincbryan@gmail.com>
2020-05-05 14:56:51 -0700

    Added player biting

---
+++ Assets/Scripts/AmIBit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmIBit : MonoBehaviour
+{
+    public IntReference score;
+    public IntReference disatisfaction;
+    public IntReference disGain;
+    public GameObject handBit;
+    
+    private void OnTriggerEnter(Collider other) 
+    {
+       if (other.tag == "Biting")
+       {
+           score.Value++;
+           disatisfaction.Value += disGain.Value;
+           Destroy(handBit);
+
+       }   
+    }
+}
