ca3649d3e47652e5fb68cd13f84996c63fc1ccdc
kevincbryan <kevincbryan@gmail.com>
2020-05-05 19:43:54 -0700

    Added Ball Bounce (now Fish)

---
+++ Assets/FishSlap.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSlap : MonoBehaviour
+{
+    public IntReference disatisfaction;
+    public IntReference disChange;
+    public FloatReference recharge;
+    private float mTime;
+    public Animator mAnimation;
+
+    private void Start() 
+    {
+        mTime = recharge.Value;    
+    }
+    private void Update() 
+    {
+        mTime += Time.deltaTime;
+
+    }
+
+    private void OnTriggerStay(Collider other) 
+    {
+        if (other.tag == "Biting" && mTime >= recharge.Value)
+        {
+            mTime = 0;
+            disatisfaction.Value -= disChange.Value;
+            mAnimation.SetBool("FishPressed", true);
+            Invoke ("ResetFish", 2F);
+
+        }
+    }
+
+    void ResetFish()
+    {
+        mAnimation.SetBool("FishPressed", false);
+    }
+}
