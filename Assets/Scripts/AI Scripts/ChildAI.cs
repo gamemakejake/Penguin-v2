fa33f72062ff3fc4cef5228c02757a72dddeffb6
kevincbryan <kevincbryan@gmail.com>
2020-05-05 17:07:16 -0700

    Added error protection to Child AI

--- Assets/Scripts/AI Scripts/ChildAI.cs
+++ Assets/Scripts/AI Scripts/ChildAI.cs
@@ -50,11 +50,19 @@ public class ChildAI : MonoBehaviour
                 {
                     if(random == i)
                     {
-                        agent.SetDestination(spots[i].position);
+                        if (agent)
+                        {
+                            agent.SetDestination(spots[i].position);
+                        }
+                        
                     }
                 
                 }
-                person.atGate = false;
+                if (person)
+                {
+                    person.atGate = false;
+                }
+                
                 timer = 10;
             }
             else
