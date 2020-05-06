76acc33a9036e23292709176f2395fcded2336cc
kevincbryan <kevincbryan@gmail.com>
2020-05-05 20:50:50 -0700

    Added hook for penguin suinds

--- Assets/NowKiss.cs
+++ Assets/NowKiss.cs
@@ -8,6 +8,7 @@ public class NowKiss : MonoBehaviour
     public IntReference dissAmount;
     bool isHit = false;
     public Rigidbody mRB;
+    public GameObject mSound;
 
    private void OnTriggerStay(Collider other) 
    {
@@ -16,6 +17,7 @@ public class NowKiss : MonoBehaviour
            isHit = true;
            dissatisfaction.Value -= dissAmount.Value;
            mRB.AddForce(new Vector3(5, 7, 13), ForceMode.Impulse);
+           mSound.SetActive(true);
            Destroy(this.gameObject, 3f);
 
        }
